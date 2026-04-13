ce7a247719f2357634f1c30dc3f0c804671b5787
SargentoAldea <julianchirino05@gmail.com>
2026-04-13 13:55:20 -0400

    Forma
    La organización de carpetas para componentes reutilizables como el navbar y el footer.
    Contiene la distribución para las paginas de inicio y demás, por ultimo contiene services y models, para poder filtrar, buscar datos, y simular datos (dado que por el momento no tocaremos nada de Backend)

---
+++ src/app/pages/species-d/species-d.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-species-d',
+  standalone: true,
+  imports: [],
+  templateUrl: './species-d.component.html',
+  styleUrl: './species-d.component.css'
+})
+export class SpeciesDComponent {
+
+}
