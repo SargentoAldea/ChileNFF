736362f190bef72267553c17e23a8b4d3e91b88f
SargentoAldea <julianchirino05@gmail.com>
2026-04-13 14:50:58 -0400

    Arreglo de Rutas y preparación del Navbar
    Corrección en errores de imports y de rutas, colocación de navbar (reciclado de actividades en clase)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { ContactComponent } from './pages/contact/contact.component'
+import { SpeciesDComponent } from './pages/species-d/species-d.component';
+import { SpeciesLComponent } from './pages/species-l/species-l.component';
+
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'catalogo', component: CatalogoComponent },
+  {path: 'contacto', component: ContactComponent},
+  {path: 'especies', component : SpeciesLComponent},
+  {path: 'especies/:id', component: SpeciesDComponent},
+];
