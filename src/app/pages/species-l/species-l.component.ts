449a5956959cdc751e3ae229049e1cab755e2a30
SargentoAldea <julianchirino05@gmail.com>
2026-04-13 22:34:53 -0400

    Toques para Especies
    Estilos y  html para el funcionamiento de Especie y su respectivo detalle, haciendo uso de Models y services para datos de prueba debido a que no se tocara nada de Backend

--- src/app/pages/species-l/species-l.component.ts
+++ src/app/pages/species-l/species-l.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpeciesService } from '../../services/species.service';
+import { Especie } from '../../models/especies.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-species-l',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './species-l.component.html',
   styleUrl: './species-l.component.css'
 })
-export class SpeciesLComponent {
+export class SpeciesLComponent implements OnInit {
+  listaEspecies: Especie[] = [];
 
+  constructor(private speciesService: SpeciesService) {}
+
+  ngOnInit() {
+    this.listaEspecies = this.speciesService.getEspecies();
+
+  }
+
+  ordenarAlfabeticamente (){
+    this.listaEspecies.sort((a,b) => a.nombre.localeCompare(b.nombre))
+  }
 }
