3843def836b98fa3f2188795e3ede7d4d8d03b08
SargentoAldea <julianchirino05@gmail.com>
2026-04-13 22:47:44 -0400

    Services + model + html + css
    Todo lo necesario para que funcione la pagina de catalogo, desde los datos de prueba, hasta el html y alguno que otro estilo

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { RouterLink } from '@angular/router'; 
+import { ProductService } from '../../services/producto.service'; 
+import { Producto } from '../../models/producto.model'; 
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [],
+
+  imports: [CommonModule, RouterLink], 
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
-export class CatalogoComponent {
+export class CatalogoComponent implements OnInit {
+  listaProductos: Producto[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+
+    this.listaProductos = this.productService.getProductos();
+  }
 }
